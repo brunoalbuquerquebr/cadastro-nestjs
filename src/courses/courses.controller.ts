7a6003f12adea36d80ae76372d5ffce04b8b3f24
Bruno Albuquerque <br.brunoalbuquerque@gmail.com>
2024-03-27 14:16:36 -0300

    cria service e modulo do curso

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,41 +1,50 @@
+import { CoursesService } from './courses.service';
 import {
   Body,
   Controller,
   Delete,
   Get,
-  HttpCode,
+  HttpException,
   HttpStatus,
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  @Get('list')
-  findAll(@Res() response) {
-    return response.status(200).send('Listagem de Cursos');
+  constructor(private readonly coursesService: CoursesService) {}
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Curso ${id}`;
+    const course = this.coursesService.findOne(id);
+
+    if (!course) {
+      throw new HttpException(
+        `Course id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Atualizado ${id}, ${JSON.stringify(body)}`;
+    return this.coursesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Remove ${id}`;
+    return this.coursesService.remove(id);
   }
 }
